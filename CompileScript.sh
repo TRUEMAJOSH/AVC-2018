ac30589f7e9e311841fd8bc4fcc7a8e49bcd470b
Tim Salisbury <salisbtimo@myvuw.ac.nz>
2018-05-07 13:14:54 +1200

    Adding compiler script

---
+++ CompileScript.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+IP=pi@10.140.30.40
+DIRPI=/home/pi/Desktop/
+DIRPC="/mnt/d/Desktop/Team4AVC2018"
+echo Deleting old c files
+sshpass -p 'raspberry' ssh -x $IP 'cd $DIRPI; rm *.cpp'
+echo Transfering files to PI
+rsync -e 'sshpass -p 'raspberry' ssh -ax' -av $DIRPC $IP:$DIRPI
+echo Compiling and Executing
+sshpass -p 'raspberry' ssh -x $IP 'cd /home/pi/Desktop/Team4AVC2018;make;echo raspberry | sudo -S ./main'
