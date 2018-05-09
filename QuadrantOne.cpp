0bf062c3ce69b225caec1903db9cbaca7a1f0b42
Tim Salisbury <salisbtimo@myvuw.ac.nz>
2018-05-09 13:44:29 +1200

    Rearranged order of code

--- QuadrantOne.cpp
+++ QuadrantOne.cpp
@@ -18,8 +18,8 @@ int get_error();
 
 time_t pTime = 0; //Previous time
 int pError = 0; //Previous error
-double Kp = 0.005; //Coefficient for proportionality
-double Kd = 0.5; //Coefficient for derivative
+double Kp = 0.005; //Coefficient for proportionality - adjustable
+double Kd = 0.5; //Coefficient for derivative - adjustable
 
 int MOTOR_LEFT = 1; //Left motor pin number
 int MOTOR_RIGHT = 2; //Right motor pin number
@@ -33,10 +33,9 @@ int quadOne(){
 
     double dE = (double)(cError - pError) / (cTime - pTime); //Gets the derivative - I think?
 
-    //Something something change motor speeds
-
-
     double motorAdjustment = cError * Kp + dE * Kd; //The final motor adjustment
+
+    //Something something change motor speeds
 
     pTime = cTime; //Assigns previous time to current time
     pError = cError; //Assigns previous error value to current error value
