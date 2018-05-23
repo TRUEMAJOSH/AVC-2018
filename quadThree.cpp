61fd5bfb629f58ecb53f689d75a2f72128d0b77f
masonyi <38927039+masonyi@users.noreply.github.com>
2018-05-24 11:37:26 +1200

    Update quadThree.cpp

--- quadThree.cpp
+++ quadThree.cpp
@@ -6,7 +6,7 @@
 void turn_left();
 void turn_right();
 void turn_around();
-bool turned_left = false;
+bool allBlack = false;
 
 int quadThree(){
 
@@ -16,12 +16,9 @@ int quadThree(){
         set_motor(MOTOR_RIGHT, 0);
         printf("returned all black");
         sleep1(1,0);
-        if(!turned_left){
-            turn_left();
-            turned_left = true;
-        }else{
-            turn_around();
-            turned_left = false;
+        allBlack = true
+        if (allBlack){
+            checkVert();
         }
         return 0;
     }
@@ -32,6 +29,32 @@ int quadThree(){
     set_motor(MOTOR_RIGHT, MOTOR_SPEED);
 
     return 0;
+}
+
+void checkVert(){   //checks vertically for anything left or right
+    printf("Checking left & right");
+    
+    int np = 0;
+    int error = 0;
+    for(int y = 0; y < 240; y++){ //Scans the entire y axis of the image on the left side @ x = 20
+        int pixel = get_pixel(y, 20, 3); //(y, x, 3)
+        // (pixels > average_brightness) will return 1 if the pixel is white or 0 of it's black
+        error += (pixel > average_brightness) * (x-160);
+        if(pixel > average_brightness){ //turns left if white detected
+            turn_left();
+        }else if(pixel < average_brightness){
+            for(int y = 0; y < 240; y++){ //Scans the entire y axis of the image on the right side @ x = 300
+                int pixel = get_pixel(y, 300, 3); //(y, x, 3)
+                // (pixels > average_brightness) will return 1 if the pixel is white or 0 of it's black
+                error += (pixel > average_brightness) * (x-160);
+                if(pixel > average_brightness){
+                    turn_right();   //turns right if white detected
+                }
+            }
+        }else {
+            turn_around();
+        }
+    }
 }
 
 void turn_left(){
